9a748946a9b38ba54945607e4d4d592dac71ea34
OutsiderVsTheWorld <nekorides@gmail.com>
2020-07-02 02:42:56 +0300

    implement simple validation for signup

--- config/passport/local-signup.js
+++ config/passport/local-signup.js
@@ -1,5 +1,6 @@
 const User = require('../../controllers/user-controller');
 const { getSignupMessage } = require('../error-messages');
+const signupValidate = require('../../lib/signup-validate');
 
 module.exports = {
   config: {
@@ -9,16 +10,30 @@ module.exports = {
   },
 
   callback(req, username, password, done) {
-    User.createUser({
-      username,
-      email: req.body.email,
-      password,
-      ip: req.ip
-    })
-      .then(res => {
-        let message = req.flash('authMessage', getSignupMessage(res));
+    signupValidate(req.body, (errors => {
+      if (errors) {
+        let errorMessages = [];
 
-        return done(null, res, { message });
-      });
+        Object.values(errors).forEach(err => {
+          errorMessages.push(err.message);
+        });
+
+        let message = req.flash('authMessage', errorMessages);
+
+        done(null, false, { message });
+      } else {
+        User.createUser({
+          username,
+          email: req.body.email,
+          password,
+          ip: req.ip
+        })
+          .then(res => {
+            let message = req.flash('authMessage', getSignupMessage(res));
+
+            done(null, res, { message });
+          });
+      }
+    }));
   }
 };
